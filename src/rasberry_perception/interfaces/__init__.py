9881f946af8bb90492b7b7d4a70beaca67e4cee6
Marc Hanheide <marc@hanheide.net>
2023-03-02 12:55:35 +0000

    disabled all other backends than default
    to make this work on noetic out of the box to play the rosbags

--- src/rasberry_perception/interfaces/__init__.py
+++ src/rasberry_perception/interfaces/__init__.py
@@ -4,8 +4,8 @@
 # Import all the interfaces here so that they're all registered
 from .registry import DETECTION_REGISTRY
 from .default import DefaultDetectionServer, default_service_name
-from .detectron2 import Detectron2Server
-from .gazebo_berries import GazeboRenderedBerriesServer
-from .fruitcast import FruitCastServer
+#from .detectron2 import Detectron2Server
+#from .gazebo_berries import GazeboRenderedBerriesServer
+#from .fruitcast import FruitCastServer
 
 __all__ = ["FruitCastServer", "GazeboRenderedBerriesServer", "Detectron2Server", "DefaultDetectionServer"]
